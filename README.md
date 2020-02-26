f6adb792638c2343c09689297fd91bdb1d1c7001
Frederik Shull <frederik.shull@protonmail.com>
2020-02-26 22:48:31 +0100

    readme fix, auto-download removed, notice message

--- README.md
+++ README.md
@@ -15,6 +15,5 @@ Serves Android Telegram Beta as .apk file to Telegram users.
 | 3      | Brazilian Portuguese | Mateus Cardoso | @mattcafe         |
 | 4      | Russian              | Kobe Black     |                   |
 | 5      | Ukrainian            | Kobe Black     |                   |
-| 6      | Persian              |                | @MrdMi            |
-| 7      | Turkish              | M.Furkan       | @By_Azade         |
-| 8      | Italian              | NotReal96      | @UnrealMatteo     |
+| 6      | Turkish              | M.Furkan       | @By_Azade         |
+| 7      | Italian              | NotReal96      | @UnrealMatteo     |
--- tgrambeta_bot.js
+++ tgrambeta_bot.js
@@ -12,107 +12,72 @@ var { users } = require("./users.json");
 var fileId = require("./fileId.json").fileId;
 downloadApk();
 setInterval(function() {
- downloadApk();
+  downloadApk();
 }, 21600000);
 
-function downloadApk() {
- let url = "https://rink.hockeyapp.net/apps/f972660267c948d2b5d04761f1c1a8f3";
- let first = "<a class='btn btn-ha-primary button' href='";
- let last = "'>";
- let first2 = '<html><body>You are being <a href="';
- let last2 = '">redirected</a>.</body></html>';
- https.get(url, response => {
-  let data = "";
-  response.on("data", chunk => {
-   data += chunk;
-  });
-  response.on("end", () => {
-   content = data.split("\n");
-   isolate = content.find(line => line.includes(first));
-   refined = isolate
-    .replace(first, "")
-    .replace(last, "")
-    .replace("&amp;", "&");
-   https.get(refined, response2 => {
-    let data = "";
-    response2.on("data", chunk => {
-     data += chunk;
-    });
-    response2.on("end", () => {
-     downloadurl = data
-      .replace(first2, "")
-      .replace(last2, "")
-      .replace("\n", "");
-     https.get(downloadurl, response3 => {
-      let apkfile = fs.createWriteStream("./tgrambeta.apk");
-      response3.pipe(apkfile);
-      response3.on("end", () => {
-       let apkfileStream = fs.createReadStream("./tgrambeta.apk");
-       bot.sendDocument(console_chat_id, apkfileStream).then(message => {
-        fileId = message.document.file_id;
-        fs.writeFileSync("./fileId.json", `{"fileId": "${message.document.file_id}"}`);
-       });
-      });
-     });
-    });
-   });
-  });
- });
-}
+function downloadApk() {}
+
 function addUser(chatId, langCode) {
- users[chatId] = langCode;
- fs.writeFileSync("./users.json", JSON.stringify({ users }));
+  users[chatId] = langCode;
+  fs.writeFileSync("./users.json", JSON.stringify({ users }));
 }
 
 bot.on("text", message => {
- const chatId = message.chat.id;
- const text = message.text;
- const langCode = users[chatId] || message.from.language_code;
- const local = locals[langCode] || locals.en;
+  const chatId = message.chat.id;
+  const text = message.text;
+  const langCode = users[chatId] || message.from.language_code;
+  const local = locals[langCode] || locals.en;
 
- bot.sendMessage(console_chat_id, `<strong>${message.chat.username}/${message.chat.first_name}/${chatId}/${langCode}</strong>: <i>${text}</i>`, {
-  parse_mode: "HTML"
- });
- addUser(chatId, langCode);
-
- switch (text) {
-  case "/start":
-   bot.sendMessage(
-    chatId,
-    Object.keys(locals)
-     .map(lang => {
-      return `${locals[lang].flag} ${locals[lang].select}`;
-     })
-     .join("\n"),
+  bot.sendMessage(
+    console_chat_id,
+    `<strong>${message.chat.username}/${message.chat.first_name}/${chatId}/${langCode}</strong>: <i>${text}</i>`,
     {
-     parse_mode: "HTML",
-     reply_markup: {
-      keyboard: Object.keys(locals).map(lang => {
-       return [{ text: `${locals[lang].flag} ${locals[lang].languagename}` }];
-      })
-     }
+      parse_mode: "HTML"
     }
-   );
-   break;
-  case local.button:
-   bot.sendMessage(chatId, local.send, { parse_mode: "HTML" });
-   bot.sendDocument(chatId, fileId);
-   break;
-  default:
-   let lang = Object.keys(locals)
-    .map(lang => {
-     return locals[lang];
-    })
-    .find(lang => `${lang.flag} ${lang.languagename}` == text);
-   if (lang) {
-    addUser(chatId, lang.code);
-    bot.sendMessage(chatId, locals[lang.code].start, {
-     parse_mode: "HTML",
-     reply_markup: {
-      keyboard: [[{ text: locals[lang.code].button }]]
-     }
-    });
-   } else bot.sendMessage(chatId, local.unknown, { parse_mode: "HTML" });
-   break;
- }
+  );
+  addUser(chatId, langCode);
+
+  // TEMP:
+  bot.sendMessage(chatId, `This bot only serves a <strong>outdated version</strong> of Telgram Beta because the download source changed.\nYou will probably still be able to update via the app itself.\nIf you want to download the newest Beta version by yourself head over to: <a href="https://install.appcenter.ms/users/drklo-2kb-ghpo/apps/telegram-beta-2/distribution_groups/all-users-of-telegram-beta-2">App Center</a>\nSorry for the inconvenience, I will try to re-add the automatic download when I have time. Feel free to checkout the <a href="https://github.com/zlyfer/TelegramGetTelegramBetaBot/tree/nodejs">GitHub Repo</a> to contibute.`, { parse_mode: "HTML" });
+
+  switch (text) {
+    case "/start":
+      bot.sendMessage(
+        chatId,
+        Object.keys(locals)
+          .map(lang => {
+            return `${locals[lang].flag} ${locals[lang].select}`;
+          })
+          .join("\n"),
+        {
+          parse_mode: "HTML",
+          reply_markup: {
+            keyboard: Object.keys(locals).map(lang => {
+              return [{ text: `${locals[lang].flag} ${locals[lang].languagename}` }];
+            })
+          }
+        }
+      );
+      break;
+    case local.button:
+      bot.sendMessage(chatId, local.send, { parse_mode: "HTML" });
+      bot.sendDocument(chatId, fileId);
+      break;
+    default:
+      let lang = Object.keys(locals)
+        .map(lang => {
+          return locals[lang];
+        })
+        .find(lang => `${lang.flag} ${lang.languagename}` == text);
+      if (lang) {
+        addUser(chatId, lang.code);
+        bot.sendMessage(chatId, locals[lang.code].start, {
+          parse_mode: "HTML",
+          reply_markup: {
+            keyboard: [[{ text: locals[lang.code].button }]]
+          }
+        });
+      } else bot.sendMessage(chatId, local.unknown, { parse_mode: "HTML" });
+      break;
+  }
 });
