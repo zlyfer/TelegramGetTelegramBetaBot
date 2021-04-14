143210c70750580ea49f2851e5a7211018d47a9a
Frederik Shull <zlyfer@pm.me>
2021-04-14 12:09:36 +0200

    prettier

--- tgrambeta_bot.js
+++ tgrambeta_bot.js
@@ -11,7 +11,7 @@ var { users } = require("./users.json");
 
 var fileId = require("./fileId.json").fileId;
 downloadApk();
-setInterval(function() {
+setInterval(function () {
   downloadApk();
 }, 21600000);
 
@@ -22,7 +22,7 @@ function addUser(chatId, langCode) {
   fs.writeFileSync("./users.json", JSON.stringify({ users }));
 }
 
-bot.on("text", message => {
+bot.on("text", (message) => {
   const chatId = message.chat.id;
   const text = message.text;
   const langCode = users[chatId] || message.from.language_code;
@@ -32,30 +32,34 @@ bot.on("text", message => {
     console_chat_id,
     `<strong>${message.chat.username}/${message.chat.first_name}/${chatId}/${langCode}</strong>: <i>${text}</i>`,
     {
-      parse_mode: "HTML"
+      parse_mode: "HTML",
     }
   );
   addUser(chatId, langCode);
 
   // TEMP:
-  bot.sendMessage(chatId, `This bot only serves a <strong>outdated version</strong> of Telgram Beta because the download source changed.\nYou will probably still be able to update via the app itself.\nIf you want to download the newest Beta version by yourself head over to: <a href="https://install.appcenter.ms/users/drklo-2kb-ghpo/apps/telegram-beta-2/distribution_groups/all-users-of-telegram-beta-2">App Center</a>\nSorry for the inconvenience, I will try to re-add the automatic download when I have time. Feel free to checkout the <a href="https://github.com/zlyfer/TelegramGetTelegramBetaBot/tree/nodejs">GitHub Repo</a> to contibute.`, { parse_mode: "HTML" });
+  bot.sendMessage(
+    chatId,
+    `This bot only serves a <strong>outdated version</strong> of Telgram Beta because the download source changed.\nYou will probably still be able to update via the app itself.\nIf you want to download the newest Beta version by yourself head over to: <a href="https://install.appcenter.ms/users/drklo-2kb-ghpo/apps/telegram-beta-2/distribution_groups/all-users-of-telegram-beta-2">App Center</a>\nSorry for the inconvenience, I will try to re-add the automatic download when I have time. Feel free to checkout the <a href="https://github.com/zlyfer/TelegramGetTelegramBetaBot/tree/nodejs">GitHub Repo</a> to contibute.`,
+    { parse_mode: "HTML" }
+  );
 
   switch (text) {
     case "/start":
       bot.sendMessage(
         chatId,
         Object.keys(locals)
-          .map(lang => {
+          .map((lang) => {
             return `${locals[lang].flag} ${locals[lang].select}`;
           })
           .join("\n"),
         {
           parse_mode: "HTML",
           reply_markup: {
-            keyboard: Object.keys(locals).map(lang => {
+            keyboard: Object.keys(locals).map((lang) => {
               return [{ text: `${locals[lang].flag} ${locals[lang].languagename}` }];
-            })
-          }
+            }),
+          },
         }
       );
       break;
@@ -65,17 +69,17 @@ bot.on("text", message => {
       break;
     default:
       let lang = Object.keys(locals)
-        .map(lang => {
+        .map((lang) => {
           return locals[lang];
         })
-        .find(lang => `${lang.flag} ${lang.languagename}` == text);
+        .find((lang) => `${lang.flag} ${lang.languagename}` == text);
       if (lang) {
         addUser(chatId, lang.code);
         bot.sendMessage(chatId, locals[lang.code].start, {
           parse_mode: "HTML",
           reply_markup: {
-            keyboard: [[{ text: locals[lang.code].button }]]
-          }
+            keyboard: [[{ text: locals[lang.code].button }]],
+          },
         });
       } else bot.sendMessage(chatId, local.unknown, { parse_mode: "HTML" });
       break;
